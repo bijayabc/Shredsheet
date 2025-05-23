03d488da8687d4a55658cc00a95ca8ba05af1e16
bijayabc <bijayabc1234@gmail.com>
2025-05-23 17:21:01 -0400

    made routineform title required

--- src/components/RoutineForm.jsx
+++ src/components/RoutineForm.jsx
@@ -76,6 +76,7 @@ const RoutineForm = () => {
             <label className="block text-sm font-medium text-gray-700">Routine Name</label>
             <input
               type="text"
+              required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-8 pl-2"
               placeholder="e.g., Chest and Shoulders I"
               onChange={(e) => setRoutineData({ ...routineData, title: e.target.value })}
